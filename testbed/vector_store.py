4b950dee396d0aefdee6468b71696b37d099336f
polaris0208 <sh6771@gmail.com>
2024-11-14 20:40:25 +0900

    feat : 임베딩 기능
    가공된 Document 객체를 임베딩 모델로 변형 후 vectorstore 형태로 저장하는 기능
    
    테스트 전

--- testbed/vector_store.py
+++ testbed/vector_store.py
@@ -29,14 +29,11 @@ splits = recursive_splitter.split_documents(docs)
 import os
 import openai
 
-# pip install openai / openai-1.54.4
-
 from langchain_openai import ChatOpenAI
 
-# pip install langchain-openai / langchain-openai-0.2.8
-
-openai.api_key = os.environ.get("________")
 # 환경 변수에서 호출
+openai.api_key = os.environ.get("________")
+
 
 model = ChatOpenAI(model="gpt-4", api_key=openai.api_key)
 
@@ -46,16 +43,9 @@ embedding_model = OpenAIEmbeddings(
     model="text-embedding-ada-002", api_key=openai.api_key
 )
 
-# pip install faiss-cpu / faiss-cpu-1.9.0
 import faiss
 from langchain_community.vectorstores import FAISS
 from langchain_community.docstore.in_memory import InMemoryDocstore
-
-# FAISS 객체 초기화 방식 - 더 많은 설정, 대용량 문서 처리가 요구되거나 성능을 최대한으로 끌어올려야 할 상황
-# 인덱스나, 임베딩이 계산되어 있는 경우 빠르게 진행
-#
-# docstore :각 벡터와 연관된 문서 데이터를 메모리에 저장
-# index_to_docstore_id: 특정 벡터가 어떤 문서와 연관되는지를 저장
 
 index = faiss.IndexFlatL2(len(embedding_model.embed_query("hello world")))
 vector_store = FAISS(
@@ -65,15 +55,9 @@ vector_store = FAISS(
     index_to_docstore_id={},
 )
 
-# from_documents 방식 - 간단하고 빠름, 일반적인 상황
 vectorstore = FAISS.from_documents(documents=splits, embedding=embedding_model)
 
-# pip install uuid / uuid-1.30
 from uuid import uuid4
-
-# 고유한 UUID(Universally Unique Identifier)를 생성
-# 128비트 숫자로, 이를 통해 거의 중복 없이 고유한 식별자를 생성
-# 여러 문서를 처리하거나 분석하는 과정에서, 각 문서를 쉽게 추적하고 연결하려면 고유 식별자가 중요
 
 for split in splits:
     split.metadata["uuid"] = str(uuid4())
@@ -88,5 +72,3 @@ results_with_scores = vector_store.similarity_search_with_score(
 )
 for res, score in results_with_scores:
     print(f"* [SIM={score:.3f}] {res.page_content} [{res.metadata}]")
-
-
