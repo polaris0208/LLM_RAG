c54a1315bfcbe695d48f3f60679718d57fddacf2
polaris0208 <sh6771@gmail.com>
2024-11-17 20:37:57 +0900

    test :2차 작동 평가
    각 기능들을 패키지화 하여 작동하는지 시험
    
    ref : 테스트 결과
    
    """
    Load PDF.....
    Load Complete....!
    Split docs.....
    Split Complete....!
    
    Authenticate API KEY....
    Authenticate Complete....!
    Set up Embedding model....
    Set up Complete....!
    Initiate FAISS instance....
    Return Vector Store....!
    
    Debug Output: RAG에 대해서 설명해주세요
    final response : RAG은 Retrieval Augmented Generation의 약자로, 초거대 언어모델 연구 동향 중 하나이다.
    """

---
+++ second_lot.py
@@ -0,0 +1,24 @@
+from RAG_Module.RAG_Params import RAGParams
+from RAG_Module.PDF_Loader import PDFLoader
+from RAG_Module.VecotorStore_Utils import VectorStoreReturn
+from RAG_Module.RAG_Chain import RAGChainMake
+
+params = RAGParams(
+    KEY= "NBCAMP_01",
+    EBD_MODEL="text-embedding-ada-002",
+    LLM_MODEL='gpt-3.5-turbo',
+    PDF_PATH="documents/초거대 언어모델 연구 동향.pdf",
+    SAVE_PATH = None,
+    IS_SAFE=True,
+    CHUNK_SIZE = 100,
+    CHUNK_OVERLAP = 10
+)
+
+docs = PDFLoader(params)
+vector_store = VectorStoreReturn(docs, params)
+chatbot_mk1 = RAGChainMake(vector_store, params)
+
+question = 'RAG에 대해서 설명해주세요'
+response = chatbot_mk1.invoke(question)
+
+print(response.content)
